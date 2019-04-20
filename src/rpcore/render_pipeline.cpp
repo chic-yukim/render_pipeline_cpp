f1ad2abb02f3fbfcb3a484ee8f9ba24d26b2461d
Younguk Kim <yukim@chic.re.kr>
2019-04-20 13:46:02 +0900

    Patch bluekyu/render_pipeline_cpp@7f72b9d

--- src/rpcore/render_pipeline.cpp
+++ src/rpcore/render_pipeline.cpp
@@ -440,7 +440,7 @@ void RenderPipeline::Impl::handle_window_event(const Event* ev)
     showbase_->window_event(ev);
 
     auto win = showbase_->get_win();
-    if (!win)
+    if (!(win && win->is_valid()))
         return;
 
     LVecBase2i window_dims(win->get_size());
@@ -466,7 +466,7 @@ void RenderPipeline::Impl::handle_window_event(const Event* ev)
 
     // set lens parameter after window event.
     // and set highest priority for running first.
-    showbase_->get_task_mgr()->add([this](rppanda::FunctionalTask* task) {
+    showbase_->add_task([this](rppanda::FunctionalTask* task) {
         adjust_lens_setting();
         return AsyncTask::DS_done;
     }, "RP_HandleWindowResize", -100);
